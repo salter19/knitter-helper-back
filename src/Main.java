19cf1bc93322f9ea17444c2accec9d10a132157d
salter19 <salonenterhi19@gmail.com>
2021-03-02 19:16:09 +0200

    Rename Index.java into Main.java, add package src.

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+package src;
+// To run the javadoc:
+// create html/ to root
+// javadoc -d /html Main.java
+
+/**
+ * This is the entry point class to run the program.
+ * 
+ * @author Terhi Salonen
+ * @version 1.0
+ * 
+ */
+public class Main {
+    /**
+     * Main method is the method that is used to run the program.
+     * 
+     * @param args    the command line arguments as a format
+     *                of String array.
+     */
+    public static void main(String[] args) {
+        
+    }
+}
