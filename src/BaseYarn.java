2dad6f1c68fe4bdf0f0e54bb7b8a917b163b5242
salter19 <salonenterhi19@gmail.com>
2021-03-03 09:41:41 +0200

    Add to BaseYarn: setUnitWeight(int).

--- src/BaseYarn.java
+++ src/BaseYarn.java
@@ -9,25 +9,37 @@ public abstract class BaseYarn {
      * unitWeight is the weight of yarn skein or 
      * ball in grams. It is rounded to integer value.
      */
+    private int unitWeight;
     /**
      * metrage is the approximate length in meters of yarn per
      * skein or ball. It is rounded to integer value.
      */
+    private int meterage;
     /**
      * gauge is approximation of stitches per cm with given
      * needle size. It is rounded to integer value.
      */
+    private int gauge;
     /**
      * needleSize is the suggested needle size in mm. It is 
      * a floating point value.
      */
+    private float needleSize;
     /**
      * weightType is a String that descripes the yarn in 
      * standard yarn weight term, if one can be applied.
      */
-    private int unitWeight, meterage, gauge;
-    private float needleSize;
     private String weightType;
 
+    /**
+     * sets unitWeight for yarn.
+     * @param unitWeight    rounded to integer, the value of
+     *                      given skein or ball of yarn.
+     */
+    public void setUnitWeight(int unitWeight) {
+        this.unitWeight = unitWeight;
+    }
 
 }
+
+// End of File
