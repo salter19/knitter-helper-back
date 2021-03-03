46d78bafb350bb8cfade18ab19f83b313b8c8b59
salter19 <salonenterhi19@gmail.com>
2021-03-03 15:18:33 +0200

    Add to Main: more test lines.

--- src/Main.java
+++ src/Main.java
@@ -17,7 +17,6 @@ public class Main {
     public static void main(String[] args) {
 
         BaseYarn lace = new Lace();
-        lace.setYarnType();
-        
+        System.out.println("This is " + lace);
     }
 }
