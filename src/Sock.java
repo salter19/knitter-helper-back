6cbe7125e8eb78ce089406f1dd8066804d618e29
salter19 <salonenterhi19@gmail.com>
2021-03-03 18:18:55 +0200

    Add to Sock: set YarnType at constructor.

--- src/Sock.java
+++ src/Sock.java
@@ -1,5 +1,16 @@
+/**
+ * Sock is a yarn type based.
+ * 
+ * Yarn types are based on the thickness of the yarn.
+ * 
+ * @author Terhi Salonen
+ * @version 1.0
+ */
 class Sock extends BaseYarn {
 
+    /**
+     * are standard default values for sock type of yarn.
+     */
     private final int DEFAULT_METERAGE = 420;
     private final int MIN_STITCH_COUNT = 27;
     private final int MAX_STITCH_COUNT = 32;
@@ -8,11 +19,15 @@ class Sock extends BaseYarn {
     private final double[] NEEDLE_SIZE = 
         {this.MIN_NEEDLE_SIZE, this.MAX_NEEDLE_SIZE};
 
+    /**
+     * is the constructor.
+     */
     public Sock() {
         setUnitWeight(DEFAULT_WEIGHT);
         setMeterage(this.DEFAULT_METERAGE);
         setGaugeForTenCm(this.MIN_STITCH_COUNT, this.MAX_STITCH_COUNT);
         setNeedleSize(this.NEEDLE_SIZE);
+        setYarnType();
     }    
 }
 
