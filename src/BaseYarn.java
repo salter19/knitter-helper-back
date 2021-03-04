326a9b181c4d1023cf94476c0b9d21b3ee22f1d5
salter19 <salonenterhi19@gmail.com>
2021-03-04 11:05:40 +0200

    Add to BaseYarn: private attributes weight and meterage.

--- src/BaseYarn.java
+++ src/BaseYarn.java
@@ -17,4 +17,17 @@ public abstract class BaseYarn {
      * a skein of yarn.
      */
     public static final int DEFAULT_WEIGHT_TWENTYFIVE = 25;
+
+    /**
+     * is the weight (in grams) of the ball or skein of yarn.
+     * Value is rounded int as there is no need for more precise value.
+     */
+    private int weight;
+
+    /**
+     * is the meterage (in meters) of the ball or skein of yarn.
+     * Value is rounded to int as there is no need for more precise value.
+     */
+    private int meterage;
+
 }
