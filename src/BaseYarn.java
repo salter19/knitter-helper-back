ba47540428c612c68d4b13c92e5bfc8d2c52af6a
salter19 <salonenterhi19@gmail.com>
2021-03-04 10:43:50 +0200

    Add BaseYarn.java

---
+++ src/BaseYarn.java
@@ -0,0 +1,10 @@
+/**
+ * BaseYarn is an abstract superclass for yarn.
+ */
+public abstract class BaseYarn {
+    /**
+     * DEFAULT_WEIGHT is the standard weight in grams for a ball or skein of
+     * yarn.
+     */
+    public static final int DEFAULT_WEIGHT = 100;
+}
