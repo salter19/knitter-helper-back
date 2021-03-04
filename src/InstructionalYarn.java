bc424e136ae127b6abacd238939a5c93c8a98723
salter19 <salonenterhi19@gmail.com>
2021-03-04 16:12:43 +0200

    Add to InstructionalYarn: missing javadoc.

--- src/InstructionalYarn.java
+++ src/InstructionalYarn.java
@@ -1,5 +1,16 @@
+/**
+ * InstructionalYarn is used as the yarn given in the knitting instruction. It
+ * is inherited from BaseYarn.
+ */
 public class InstructionalYarn extends BaseYarn {
 
+    /**
+     * is the constructor.
+     * @param weight        int     The weight of the ball or skein of the yarn
+     *                              in question.
+     * @param meterage      int     The metrage of the ball or skein of the yarn
+     *                              in question.
+     */
     public InstructionalYarn(int weight, int meterage) {
         super(weight, meterage);
     }
