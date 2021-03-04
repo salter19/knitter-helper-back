b3794999b0a8b06c4c579355aa543d84c5501859
salter19 <salonenterhi19@gmail.com>
2021-03-04 12:55:00 +0200

    Add to BaseYarn: getYarnTypeBasedOnMeterage().

--- src/BaseYarn.java
+++ src/BaseYarn.java
@@ -18,6 +18,9 @@ public abstract class BaseYarn {
      */
     public static final int DEFAULT_WEIGHT_TWENTYFIVE = 25;
 
+
+    private final int UNDEFINED_INT = -1;
+
     /**
      * is the weight (in grams) of the ball or skein of yarn.
      * Value is rounded int as there is no need for more precise value.
@@ -29,5 +32,46 @@ public abstract class BaseYarn {
      * Value is rounded to int as there is no need for more precise value.
      */
     private int meterage;
+
+    /**
+     * is the YarnType of the yarn.
+     * Value is based on the thickness of the yarn (meters per 100 grams).
+     */
+    private YarnType yarnType;
+
+
+    /** Initate helper object meterageRange. */
+    private MeterageRange meterageRange = new MeterageRange();
+
+    /**
+     * is the constructor, which sets the weight and meterage of yarn in 
+     * question.
+     * @param weight        int value of weight of the ball or skein of yarn.
+     * @param meterage      int value of meterage of the ball or skein of yarn.
+     */
+    public BaseYarn(int weight, int meterage) {
+
+        this.weight = weight;
+        this.meterage = meterage;
+        this.yarnType = this.getYarnType();
+    }
+
+    /**
+     * returns YarnType based on the meterage in hundred grams.
+     * If neither weight nor meterage is set, returns YarnType.UNDEFINED.
+     * 
+     * @return  YarnType
+     */
+    private YarnType getYarnType() {
+
+        if (this.weight != this.UNDEFINED_INT 
+        && this.meterage != this.UNDEFINED_INT) {
+
+            double metersPerHundredGrams = 
+                ((double) this.meterage / this.weight) * DEFAULT_WEIGHT_HUNDRED;
+            return this.meterageRange.getYarnType((int)metersPerHundredGrams);
+        }
+        return YarnType.UNDEFINED;
+    }
 
 }
