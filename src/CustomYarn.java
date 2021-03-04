17c264ca7fbbdb536520e82bbf63800f5fc85023
salter19 <salonenterhi19@gmail.com>
2021-03-04 16:08:14 +0200

    Add to CustomYarn.java: missing javadoc.

--- src/CustomYarn.java
+++ src/CustomYarn.java
@@ -1,7 +1,17 @@
+/**
+ * CustomYarn is used as the yarn to be fitted in place of the instructional
+ * yarn. It is inherited from BaseYarn class.
+ */
 public class CustomYarn extends BaseYarn {
 
+    /**
+     * is the constructor. 
+     * @param weight    int     The weight (per ball or skein) of the yarn to be
+     *                          used.
+     * @param meterage  int     The meterage (per ball or skein) of the yarn to
+     *                          be used.
+     */
     public CustomYarn(int weight, int meterage) {
-
         super(weight, meterage);
     }
 
