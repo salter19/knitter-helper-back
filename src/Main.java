46fb2e20fcec1d9656f3a20be10b6689c186db97
salter19 <salonenterhi19@gmail.com>
2021-03-04 15:32:30 +0200

    Add to Main: test the code.

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@
  * 
  */
 public class Main {
+    
     /**
      * Main method is the method that is used to run the program.
      * 
@@ -13,7 +14,15 @@ public class Main {
      *                of String array.
      */
     public static void main(String[] args) {
+        BaseYarn instructionalYarn = 
+            new InstructionalYarn(BaseYarn.DEFAULT_WEIGHT_FIFTY, 150);
+
+        BaseYarn customYarn = 
+            new CustomYarn(BaseYarn.DEFAULT_WEIGHT_TWENTYFIVE, 150);
         
+        Printer p = new Printer();
+        p.printMsg("" + instructionalYarn.getYarnType());
+        p.printMsg("" + customYarn.getYarnType());
     }
 }
 
