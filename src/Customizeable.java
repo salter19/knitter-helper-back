1389e9f2d529c685eea4ee76026f97599fca8d10
salter19 <salonenterhi19@gmail.com>
2021-03-04 15:49:43 +0200

    Add to Customizeable: missing javadocs.

--- src/Customizeable.java
+++ src/Customizeable.java
@@ -1,4 +1,11 @@
+/**
+ * Customizeable is an interface for Objects that need a custom value counter.
+ */
 public interface Customizeable {
+    /**
+     * returns custom value in typeof double.
+     * @return  double  The custom value.
+     */
     double getCustomValue();
 }
 
