31df457e044f88f281346491b3a1f6f5d567d90d
salter19 <salonenterhi19@gmail.com>
2021-03-05 10:19:33 +0200

    Add to Engine: missing javadoc.

--- src/Engine.java
+++ src/Engine.java
@@ -2,18 +2,30 @@ import java.util.*;
 
 /**
  * Engine is used to run the UI, and the program at CLI.
+ * 
+ * @author Terhi Salonen
+ * @version 1.0
  */
 public class Engine {
 
+    /** Scanner used for CLI input */
     private Scanner scanner;
+
+    /** Printer used for CLI output */
     private Printer printer;
 
+    /**
+     * is constructor.
+     */
     public Engine() {
         this.scanner = new Scanner(System.in);
         this.printer = new Printer();
     }
 
 
+    /**
+     * is used to run the program until user chooses to close it.
+     */
     public void run() {
         boolean isOn = true;
 
@@ -21,6 +33,8 @@ public class Engine {
             printer.printMsg("Revving");
             isOn = false;
         }
+        scanner.close();
+        System.exit(0);
     }
 }
 
