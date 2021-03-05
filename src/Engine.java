c59601c09c3f0e51dcc067627070a3fa61c2eac6
salter19 <salonenterhi19@gmail.com>
2021-03-05 12:08:41 +0200

    Add to Engine: checkInputForExit(boolean, String).

--- src/Engine.java
+++ src/Engine.java
@@ -14,12 +14,30 @@ public class Engine {
     /** Printer used for CLI output */
     private Printer printer;
 
+    /** User's options */
+    private final String CHOICES_MSG = "Your choices are:\n" 
+                                    + "[1] Get custom stitch count\n" 
+                                    + "[2] Get width\n" 
+                                    + "[3] Get yarn type\n" 
+                                    + "[x] Close program";
+    private final String[] options_arr = { "x", "1", "2", "3" };
+    private final List<String> options = new ArrayList<String>();
+
     /**
      * is constructor.
      */
     public Engine() {
         this.scanner = new Scanner(System.in);
         this.printer = new Printer();
+        this.setOptionsArrayList();
+    }
+
+
+    private void setOptionsArrayList() {
+
+        for(String s : this.options_arr) {
+            this.options.add(s);
+        }
     }
 
 
@@ -28,13 +46,49 @@ public class Engine {
      */
     public void run() {
         boolean isOn = true;
+        String input = "";
+
+        printer.printMsg("Hello!\nThis is Stitch Counter 1.0\n");
+        printer.printMsg(this.CHOICES_MSG);
 
         while (isOn) {
-            printer.printMsg("Revving");
-            isOn = false;
+            printer.printMsg("What do you want to do?");
+            printer.printMsgWithoutLn("> ");
+
+            input = scanner.nextLine();
+
+            isOn = checkInputForExit(isOn, input); 
+            
+            checkInputForNonCommand(input);
         }
         scanner.close();
         System.exit(0);
+    }
+
+
+    private void checkInputForNonCommand(String input) {
+        if (!this.options.contains(input) 
+            && !input.equals(this.options.get(0).toUpperCase())) {
+
+            printer.printMsg("Invalid command, cannot compute.");
+            return;
+        }
+    }
+
+
+    /**
+     * checks whether the user wants to close the program.
+     * @param isOn      boolean     Changes according to user input. State of
+     *                              isOn is the value to be returned. 
+     * @param input     String      User input.
+     * @return          boolean     State of isOn after input valuation.
+     */
+    private boolean checkInputForExit(boolean isOn, String input) {
+        if ( input.equalsIgnoreCase(this.options.get(0)) ) {
+            printer.printMsg("closing with command...");
+            isOn = false;
+        }
+        return isOn;
     }
 }
 
