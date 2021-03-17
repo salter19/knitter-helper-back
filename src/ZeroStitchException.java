5da193f1220527ac784f8a2b82669046ef2cdfd0
salter19 <salonenterhi19@gmail.com>
2021-03-17 15:23:11 +0200

    Add ZeroStitchException.java

---
+++ src/ZeroStitchException.java
@@ -0,0 +1,26 @@
+/**
+ * ZeroStitchException is thrown when stitch count given is zero.
+ */
+class ZeroStitchException extends Exception {
+
+    /** detail is the message to be printed when this exception is thrown */
+    String detail;
+
+    /**
+     * constructor that takes in a custom message
+     * @param message   String  The custom message.
+     */
+    public ZeroStitchException(String message) {
+        this.detail = message;
+    }
+
+    /**
+     * is used when exception has been caught
+     * @return String   this.detail as a exception message
+     */
+    @Override
+    public String getMessage() {
+        return this.detail;
+    }
+    
+}
